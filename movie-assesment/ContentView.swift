f38891a2e82e76855c9daeb8cc0477b7a62d5fdc
umaaamm <umam.tekno@gmail.com>
2023-02-01 09:10:29 +0700

    Initial Commit

---
+++ movie-assesment/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  movie-assesment
+//
+//  Created by Kurniawan Gigih Lutfi Umam on 01/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ movie-assesment/movie_assesmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_assesmentApp.swift
+//  movie-assesment
+//
+//  Created by Kurniawan Gigih Lutfi Umam on 01/02/23.
+//
+
+import SwiftUI
+
+@main
+struct movie_assesmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
